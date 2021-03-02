84782c6a236a56790213745cdc21a6bc642ae0b0
YohanTheo972 <72983311+YohanTheo972@users.noreply.github.com>
2021-03-02 16:04:47 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,405 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>Mon Portfolio</title>
+  <link href="styles.css" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<script src='https://kit.fontawesome.com/a076d05399.js'></script>  
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <strong><a class="navbar-brand" href="#">Mon Portfolio</a></strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+         &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      </div>
+      <div class=".topnav-centered a">
+      <ul class="nav navbar-nav">
+        <li class="#présentation"><a href="#">Présentation</a></li>
+        <li><a href="#mes-compétences">Mes Compétences</a></li>
+        <li><a href="#mes-loisirs">Mes Loisirs</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul></div>
+    </div>
+  </nav>
+
+  <div class="box">
+  <style>
+    .fa {
+      padding: 1px;
+      font-size: 20px;
+      width: 35px;
+      text-align: center;
+      text-decoration: none;
+      margin: 5px 2px;
+    }
+    
+    .fa:hover {
+        opacity: 0.7;
+    }
+    
+    .fa-facebook {
+      background: #ffffff;
+      color: rgb(0, 0, 0);
+    }
+    
+    .fa-twitter {
+      background: #ffffff;
+      color: rgb(0, 0, 0);
+    }
+    
+    .fa-google {
+      background: #ffffff;
+      color: rgb(0, 0, 0);
+    }
+    
+    .fa-linkedin {
+      background: #ffffff;
+      color: rgb(0, 0, 0);
+    }
+    
+    .fa-youtube {
+      background: #ffffff;
+      color: rgb(0, 0, 0);
+    }
+    }
+    </style>
+    </head>
+    <body>
+    
+    
+    <a href="https://www.facebook.com/nolan.ricky.332/" class="fa fa-facebook"></a>
+    <a href="https://twitter.com/TheoAxel1" class="fa fa-twitter"></a>
+    <a href="https://myaccount.google.com/?tab=kk" class="fa fa-google"></a>
+    <a href="https://www.linkedin.com/in/yoan-th%C3%A9ophile-catherine-056aa2204/" class="fa fa-linkedin"></a>
+    <a href="https://www.youtube.com/channel/UC4iCVPYitpLHBLBSXhCESfg" class="fa fa-youtube"></a>
+  
+  
+  
+
+
+
+</head>
+<body>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <div class="parallax"></div>
+    
+<style>
+    
+.parallax {
+  background-image: url("AnimatedMicrochip2.gif");
+
+  min-height: 270px; 
+
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+</style>
+
+<br>
+<marquee><h3>La réussite n'arrive que par l'effort et le travail !</h3></marquee>
+
+<center><strong><?php
+$date = date("d-m-Y");
+$heure = date("H:i");
+Print("Aujourd'hui nous sommes le $date et il est $heure");
+?></strong></center>
+
+
+
+
+
+
+
+
+<div id="présentation"> </div>
+<hgroup>
+    <h1><strong><u>Présentation</u> :</strong></h1>
+    <h3><em><strong>Je suis Yoan THEOPHILE-CATHERINE</strong></em></h3>
+</hgroup>
+
+
+<p><h4>Comme indiquée précédement, je suis Yoan THEOPHILE-CATHERINE, j'ai 21 ans et je suis actuellement une formation de Développement web et web mobiles au Régiment du Service MIlitaire Adapté de la Martinique (RSMAM).</h4></p>
+
+<aside>
+    <p><H4><center>Mon ambition est d'être polyvalent dans le domaine du web.</center></H4></p>
+</aside>
+
+<p><h4>Je souhaite être formé dans les domaines de la conception et du développement de site web ainsi que dans le design web. Maîtrise de plusieurs types de langages informatiques et capable de travailler sur différentes plateformes. Autonome et capable de travailler en équipe. J’ai le sens des responsabilités. Grande disponibilité.</h4></p>
+
+    
+
+<hr>
+
+<div id="mes-compétences"> </div>
+<hgroup>
+  <h1><strong><u>Mes compétences</u> :</strong></h1>
+  <h3><em><strong>Présentation de mes compétences acquise</strong></em></h3>
+</hgroup><br>
+
+<h2><center>Voici une vidéo de présentation de mes compétences, elle vous sera plus parlante.</center></h2>
+<center><iframe width="1120" height="710" src="https://www.youtube.com/embed/rvRNrtRwlV8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+
+<hr>
+
+<div id="mes-loisirs"> </div>
+<hgroup>
+  <h1><strong><u>Mes Loisirs</u> :</strong></h1>
+  <h3><em><strong>Voici mes activités extra-professionnelles</strong></em></h3>
+</hgroup>
+
+<div class="para">
+<li><i class='fas fa-dumbbell' style='font-size:48px;color:red'></i>&nbsp; &nbsp;&nbsp; &nbsp;<u>Musculation :</u>&nbsp; &nbsp;  Je pratique la musculation durant mes activité extra-professionnelle.</li><br>
+
+<li><i class='fas fa-basketball-ball' style='font-size:48px;color:red'></i>&nbsp; &nbsp; &nbsp; &nbsp;<u>Basket-ball :</u>&nbsp; &nbsp;  Quelques fois je pratique du basket-ball en groupe avec d'autres personnes en faisant des rencontres amicales.</li><br>
+
+<li><i class='fas fa-users' style='font-size:48px;color:red'></i>&nbsp; &nbsp; &nbsp; &nbsp;<u>Activités en groupe :</u>&nbsp; &nbsp; Je me réunis avec plusieurs jeunes à une dates précise pour effectué des activités diverse.</li><br>
+
+<hr>
+
+<div id="contact"> </div>
+<hgroup>
+  <h1><strong><u>Prenez contact avec moi</u> :</strong></h1>
+  <h3><em><strong>Pour des d'informations complémentaire</strong></em></h3>
+</hgroup>
+
+<style>
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  
+  * {
+    box-sizing: border-box;
+  }
+  
+  /* Style inputs */
+  input[type=text], select, textarea {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    margin-top: 6px;
+    margin-bottom: 16px;
+    resize: vertical;
+  }
+  
+  input[type=submit] {
+    background-color: teal;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    cursor: pointer;
+  }
+  
+  input[type=submit]:hover {
+    background-color: teal;
+  }
+  
+  /* Style the container/contact section */
+  .container {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 10px;
+  }
+  
+  /* Create two columns that float next to eachother */
+  .column {
+    float: left;
+    width: 50%;
+    margin-top: 6px;
+    padding: 20px;
+  }
+  
+  /* Clear floats after the columns */
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+  
+  /* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+  @media screen and (max-width: 600px) {
+    .column, input[type=submit] {
+      width: 100%;
+      margin-top: 0;
+    }
+  }
+  </style>
+  </head>
+  <body>
+  
+  
+  <div class="container">
+    <div style="text-align:center">
+      <h2>Contactez-moi</h2>
+      <p><u>Prenez contact avec moi pour plus d'informations :</u></p>
+    </div><br>
+
+
+<h5><i class='fas fa-map-marker' style='font-size:24px'></i>&nbsp; Quartier place d'Armes, 97232, Le Lamentin<br><br>
+<i class='fas fa-phone' style='font-size:24px'></i>&nbsp; Fixe: 0596 196 115 / Portable: 0696 196 115<br><br>
+<i class='fas fa-globe' style='font-size:24px'></i>&nbsp; E-mail: yohantheocomptepro@outlook.fr</h5><br><br>
+
+<div class="hola">
+<marquee><p>Ou visiter le site de ma mini-entreprise :&nbsp; &nbsp;<a href="https://web-up-design.jimdosite.com/">Web UP Design</a><br><br></marquee>
+</div>
+    <form method="post">
+        <label>Nom</label>
+        <input type="text" name="nom" required>
+        <label>Email</label>
+        <input type="email" name="email" required>
+        <br><br><label>Message</label>
+        <textarea name="message" required></textarea>
+        <input type="submit">
+
+    <?php
+    if(isset($_POST['message'])){
+        $entete  = 'MIME-Version: 1.0' . "\r\n";
+        $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+        $entete .= 'From: ' . $_POST['email'] . "\r\n";
+
+        $message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
+        <p><b>Nom : </b>' . $_POST['nom'] . '<br>
+        <b>Email : </b>' . $_POST['email'] . '<br>
+        <b>Message : </b>' . $_POST['message'] . '</p>';
+
+        $retour = mail('yohantheocomptepro@outlook.fr', 'Envoi depuis page Contact', $message, $entete);
+        if($retour) {
+            echo '<p>Votre message a bien été transmis.</p>';
+        }
+    }
+    ?>
+  </form>
+  </form>
+</div>
+</div>
+
+
+
+
+
+
+<style>
+    .mySlides {display:none;}
+    </style>
+
+    
+    <h2 class="w3-center">Mes travaux réaliser</h2>
+
+    <center>Voici une galerie d'images présentant mes travaux réaliser.</center>
+    
+    <div class="w3-content w3-display-container">
+      
+        <img class="mySlides" src="Capture d’écran du 2021-03-02 10-35-34.png" style="width:100%">
+        <img class="mySlides" src="Capture d’écran du 2021-03-02 10-36-18.png" style="width:100%">
+        <img class="mySlides" src="ihoupyoyço.png" style="width:100%">
+        <img class="mySlides" src="fy,fykyxrjryjrr.png" style="width:100%">
+        <img class="mySlides" src="Untitled (1).png" style="width:100%">
+        <img class="mySlides" src="maquette twitter.png" style="width:100%">
+    
+      <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+      <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+    </div>
+    
+    <script>
+    var slideIndex = 1;
+    showDivs(slideIndex);
+    
+    function plusDivs(n) {
+      showDivs(slideIndex += n);
+    }
+    
+    function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+      }
+      x[slideIndex-1].style.display = "block";  
+    }
+    </script>
+</p>
+</div>
+    
+<style>
+ 
+  
+  #myBtn {
+    display: none;
+    position: fixed;
+    bottom: 20px;
+    right: 30px;
+    z-index: 99;
+    font-size: 18px;
+    border: none;
+    outline: none;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+    padding: 18px;
+    border-radius: 50px;
+  }
+  
+  #myBtn:hover {
+    background-color: #555;
+  }
+  </style>
+  </head>
+  <body>
+  
+  <button onclick="topFunction()" id="myBtn" title="Go to top">HAUT</button>
+
+  
+  <script>
+  //Get the button
+  var mybutton = document.getElementById("myBtn");
+  
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  </script>
+  
+
+  
+    
+
+
+</body>
+
+<footer>
+  <strong><h5><center>Portfolio réalisé par © WebUP Design 2021 Tous droit réservés</center></h5></strong>
+</footer>
+</html>
